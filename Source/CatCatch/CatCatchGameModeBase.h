b18a7a7ac6e6b6b8056b7045d74a320ac174ccce
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2020-03-28 18:04:40 -0500

    files

---
+++ Source/CatCatch/CatCatchGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CatCatchGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CATCATCH_API ACatCatchGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
